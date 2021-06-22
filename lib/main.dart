ac9f2b41fb8c134688d493605cc2084fd5ffe07a
Sagar naik <sagarnaikg00@gmail.com>
2021-06-22 09:57:32 +0530

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:heart_beat_monitor/constants/theme.dart';
 import 'package:heart_beat_monitor/pages/home_page/screen.dart';
-import 'package:logger/logger.dart';
 
 void main() {
   runApp(MyApp());
